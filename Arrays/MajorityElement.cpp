48934d7ffbc9cc33b0b52b8d613307a3e3f071d5
Soham <sohamkalg@gmail.com>
2026-05-26 13:48:54 +0530

    Done few string

--- Arrays/MajorityElement.cpp
+++ Arrays/MajorityElement.cpp
@@ -31,3 +31,29 @@ int main()
         
         return 0;
 }
+/*
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+
+        int count = 0;
+        int candidate = 0;
+
+        for(int num : nums) {
+
+            if(count == 0) {
+                candidate = num;
+            }
+
+            if(num == candidate) {
+                count++;
+            }
+            else {
+                count--;
+            }
+        }
+
+        return candidate;
+    }
+};
+*/
